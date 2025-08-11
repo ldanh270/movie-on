ee17b3a99dd254b8aa5e5f23e0e5740d9c629f65
ldanh270 <ducanhle.dn@gmail.com>
2025-08-11 13:35:57 +0700

    feat: add theme + darkmode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,51 +1,47 @@
-import type { Metadata } from "next";
-import { Bebas_Neue, Oswald, Source_Sans_3 } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from '@/components/theme-provider'
+
+import type { Metadata } from 'next'
+import { Bebas_Neue, Oswald, Source_Sans_3 } from 'next/font/google'
+
+import './globals.css'
 
 const text = Source_Sans_3({
-  subsets: ['vietnamese','latin'],
-  weight: '400',
-  display: 'swap',
-  variable: '--fontText'
+    subsets: ['vietnamese', 'latin'],
+    weight: '400',
+    display: 'swap',
+    variable: '--fontText',
 })
 
 const title = Oswald({
-  subsets: ['vietnamese','latin'],
-  weight: '600',
-  display: 'swap',
-  variable: '--fontTitle'
+    subsets: ['vietnamese', 'latin'],
+    weight: '600',
+    display: 'swap',
+    variable: '--fontTitle',
 })
 
-
 export const metadata: Metadata = {
-  title: "MovieOn",
-  description: "An online cinema website. Create by ldanh270",
-};
+    title: 'MovieOn',
+    description: 'An online cinema website. Create by ldanh270',
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        
-      </head>
-      <body
-        className={`${text.variable} ${title.variable}`}
-      >
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-        >
-          
-        {children}
-          </ThemeProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <head></head>
+            <body className={`${text.variable} ${title.variable}`}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 
 export default function Home() {
-	return (
-		<div>
-			<h1>Homepage</h1>
-		</div>
-	)
+    return (
+        <div>
+            <h1>Homepage</h1>
+        </div>
+    )
 }
