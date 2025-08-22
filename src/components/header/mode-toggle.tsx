725f07b9bcc38412ca000a8f2fa555f8603507b1
ldanh270 <ducanhle.dn@gmail.com>
2025-08-22 22:51:01 +0700

    style: restyle darkmode colors

--- src/components/header/mode-toggle.tsx
+++ src/components/header/mode-toggle.tsx
@@ -23,7 +23,7 @@ export function ModeToggle() {
                 <Button
                     variant="outline"
                     size="icon"
-                    className="cursor-pointer border hover:bg-input"
+                    className="cursor-pointer border-secondary hover:bg-input"
                 >
                     <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
                     <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
