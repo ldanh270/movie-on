26cbdb492c2c34ec66332d5c9b66afb6844e3d4d
ldanh270 <ducanhle.dn@gmail.com>
2025-08-22 10:56:53 +0700

    refactor(header): seperate logo component from header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,3 +1,4 @@
+import Logo from '@/components/header/logo'
 import { ModeToggle } from '@/components/header/mode-toggle'
 import Navbar from '@/components/header/navbar'
 import SearchBox from '@/components/header/search-box'
@@ -8,24 +9,8 @@ import Link from 'next/link'
 const Header = () => {
     return (
         <div className="flex flex-row justify-around border-1 py-2.5">
-            {/* logo */}
-            <Link href="/" className="logo flex flex-row just ify-around items-center gap-4">
-                <Image
-                    className="fill-primary"
-                    src="/logo.svg"
-                    alt="Logo"
-                    width={46}
-                    height={46}
-                    quality={100}
-                />
-                <div className="font-title text-2xl">
-                    <span className="">Movie</span>
-                    <span className="text-primary">On</span>
-                </div>
-            </Link>
-            {/* navbar */}
+            <Logo />
             <Navbar />
-
             <div className="flex justify-center items-center gap-1">
                 <SearchBox />
                 <ModeToggle />
