e400610e6c184e230b350abdb654be93cb692bdc
ldanh270 <ducanhle.dn@gmail.com>
2025-08-22 12:44:04 +0700

    fix: fix logo image display error

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -5,16 +5,16 @@ export default function Logo() {
     return (
         <Link
             href="/"
-            className="logo flex flex-row just ify-around items-center gap-4 select-none"
+            className="logo flex flex-row justify-around items-center gap-4 select-none"
             aria-disabled
         >
             <Image
-                className="fill-primary"
                 src="/logo.svg"
                 alt="Logo"
                 width={46}
                 height={46}
                 quality={100}
+                className="fill-primary"
             />
             <div className="font-title text-2xl">
                 <span className="">Movie</span>
