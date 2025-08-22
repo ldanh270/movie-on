e943005d99d43fc42eb99c71e46b42fa25990cc8
ldanh270 <ducanhle.dn@gmail.com>
2025-08-22 10:58:12 +0700

    style(header): only border bottom header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,12 +3,9 @@ import { ModeToggle } from '@/components/header/mode-toggle'
 import Navbar from '@/components/header/navbar'
 import SearchBox from '@/components/header/search-box'
 
-import Image from 'next/image'
-import Link from 'next/link'
-
 const Header = () => {
     return (
-        <div className="flex flex-row justify-around border-1 py-2.5">
+        <div className="flex flex-row justify-around border-b-1 py-2.5">
             <Logo />
             <Navbar />
             <div className="flex justify-center items-center gap-1">
