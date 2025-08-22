085d512bdce75f817943af3f42da061aec8d1aad
ldanh270 <ducanhle.dn@gmail.com>
2025-08-22 11:21:07 +0700

    fix: resolve config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,6 @@ import type { Config } from 'tailwindcss'
 
 export default {
     content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
-<<<<<<< HEAD
     theme: { extend: {} },
-=======
-    theme: {},
->>>>>>> 751947dd3f8410c15e9db6bf28d5db2169b838b2
     plugins: [],
 } satisfies Config
