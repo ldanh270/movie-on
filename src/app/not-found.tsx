e88b619a4ab3d84d82326db5acebcd91df324003
ldanh270 <ducanhle.dn@gmail.com>
2025-11-30 14:08:12 +0700

    fix(not-found): restyle for better UI

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,12 @@ import Link from "next/link"
 
 export default function NotFound() {
     return (
-        <section className="text-center3 container mx-auto grid place-items-center py-20">
+        <section className="text-center3 container mx-auto flex h-[66.5vh] flex-col place-items-center items-center py-20">
             <h1 className="p-5 text-9xl font-bold">404</h1>
-            <h2 className="text-3xl font-semibold">Page not found</h2>
-            <p className="text-muted-foreground mt-2">The page you requested does not exist.</p>
+            <h2 className="text-4xl font-semibold">Page not found</h2>
+            <p className="text-muted-foreground mt-2 text-xl">
+                The page you requested does not exist.
+            </p>
             <div className="mt-6 p-5">
                 <Link
                     href="/"
